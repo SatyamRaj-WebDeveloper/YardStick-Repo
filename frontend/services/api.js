75ffce4d218460b9a54f54037d8bed72ffe031b8
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2025-07-08 16:05:09 +0530

    changed the baseURL to the deployed Backend

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api/v1/transaction',
+  baseURL: 'https://yardstick-repo-1.onrender.com',
 });
 export const addTransaction = (data) => API.post('/addTransaction', data);
 
